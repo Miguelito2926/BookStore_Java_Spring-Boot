2e5bf8a6ebfb30606d58df2a48ef06af9b8bf57c
miguelito2926 <juniorprol@hotmail.com>
2025-04-08 10:31:50 -0300

    Testando spring security com formLogin customizado e thymeleaf criando view login, configurando controller para acesso a viewer

---
+++ src/main/java/com/ednaldo/BookStore/config/SecurityConfiguration.java
@@ -0,0 +1,26 @@
+package com.ednaldo.BookStore.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+     return    httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(configurer -> { configurer
+                        .loginPage("/login")
+                        .permitAll();
+              })
+                .authorizeHttpRequests(authorize -> {authorize.anyRequest().authenticated();
+                })
+             .build();
+    }
+}
