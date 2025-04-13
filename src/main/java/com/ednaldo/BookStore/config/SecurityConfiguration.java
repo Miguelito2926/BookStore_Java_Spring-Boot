c1975adb7ebcd93b853c8f0e675c784a11d8a7a3
miguelito2926 <juniorprol@hotmail.com>
2025-04-12 21:56:33 -0300

    Testando metodo autenticação com do spring security com usuários em memoria

--- src/main/java/com/ednaldo/BookStore/config/SecurityConfiguration.java
+++ src/main/java/com/ednaldo/BookStore/config/SecurityConfiguration.java
@@ -5,6 +5,12 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.stereotype.Component;
 
@@ -13,16 +19,40 @@ import org.springframework.stereotype.Component;
 public class SecurityConfiguration {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
-     return    httpSecurity
-             .httpBasic(Customizer.withDefaults())
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .httpBasic(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
-                .formLogin(configurer -> { configurer
-                        .loginPage("/login")
-                        .permitAll();
-              })
-                .authorizeHttpRequests(authorize -> {authorize.anyRequest().authenticated();
+                .formLogin(configurer -> {
+                    configurer
+                            .loginPage("/login")
+                            .permitAll();
                 })
-             .build();
+                .authorizeHttpRequests(authorize -> {
+                    authorize.anyRequest().authenticated();
+                })
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
+
+    @Bean
+    public UserDetailsService userDetails(PasswordEncoder encoder) {
+        UserDetails user = User.builder()
+                .username("usuario")
+                .password(encoder.encode("123"))
+                .roles("USER")
+                .build();
+
+        UserDetails adm = User.builder()
+                .username("admin")
+                .password(encoder.encode("321"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, adm);
     }
 }
